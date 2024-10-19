3d7f1dc8fe414cba6444db13bb91717198439a45
dhebeurmila <dhebeurmila0@gmail.com>
2024-10-19 19:01:34 +0530

    4,5 prog

---
+++ Test1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Test1{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x % == 0)
+        {
+            System.out.println("even no");
+        }
+        else{
+            System.out.println("odd no");
+        }
+    }
+}
---
+++ Test2.java
@@ -0,0 +1,7 @@
+public class Test2
+{
+    public static void main (String [] args){
+        String name="urmila";
+        System.out.println("my name is "+name);
+    }
+}
